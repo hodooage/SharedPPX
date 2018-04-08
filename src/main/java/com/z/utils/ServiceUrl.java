8b5365edfc910fac8721a5889d9a6918f5a5ca05
humingx <wenjie.zhang@aorise.org>
2018-04-08 17:12:02 +0800

    1.修正xml文件中的SQL语句USER为user 2.上传图片的地址改为阿里云的服务器地址

--- src/main/java/com/z/utils/ServiceUrl.java
+++ src/main/java/com/z/utils/ServiceUrl.java
@@ -2,7 +2,8 @@ package com.z.utils;
 
 public class ServiceUrl {
     public static final String PROTOCOL="http";
-    public static final String IP="192.168.1.206";
+//    public static final String IP="192.168.1.206";
+    public static final String IP="39.108.12.143";
     public static final String PORT="8080";
     public static final String PROJECTNAME="SharedPPX";
     public static final String IMAGEFILEPACKAGE="images";
